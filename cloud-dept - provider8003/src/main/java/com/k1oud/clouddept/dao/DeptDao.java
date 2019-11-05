149e95e959bc2f121a5f9840e3dfba7ed90a6381
王玕一 <872246150@qq.com>
2019-11-05 14:45:16 +0800

    搭建provider集群

---
+++ cloud-dept - provider8003/src/main/java/com/k1oud/clouddept/dao/DeptDao.java
@@ -0,0 +1,16 @@
+package com.k1oud.clouddept.dao;
+
+
+import com.k1oud.dept.entity.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DeptDao {
+    boolean addDept(Dept dept);
+
+    Dept findById(Long id);
+
+    List<Dept> findAll();
+}
---
+++ cloud-dept - provider8003/src/main/java/com/k1oud/clouddept/service/impl/DeptServiceImpl.java
@@ -0,0 +1,36 @@
+package com.k1oud.clouddept.service.impl;
+
+
+import com.k1oud.clouddept.dao.DeptDao;
+import com.k1oud.clouddept.service.DeptService;
+import com.k1oud.dept.entity.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
